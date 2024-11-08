3b34cafb0585952089ea35e96310697da3f37808
Quoc2312 <21010596@st.phenikaa-uni.edu.vn>
2024-11-08 22:56:50 +0700

    bai tap ve nha

---
+++ bai1.js
@@ -0,0 +1,43 @@
+let month = prompt("Nhap vao so thang")
+
+switch(month) {
+    case "1":
+    console.log("thang 1 co 31 ngay");
+    break;
+    case "2":
+    console.log("thang 2 co 28 ngay")
+    break;
+    case "3":
+    console.log("thang 3 co 31 ngay")
+    break;
+    case "4":
+    console.log("thang 4 co 30 ngay")
+    break;
+    case "5":
+    console.log("thang 5 co 31 ngay")
+    break;
+    case "6":
+    console.log("thang 6 co 30 ngay")
+    break;
+    case "7":
+    console.log("thang 7 co 31 ngay")
+    break;
+    case "8":
+    console.log("thang 8 co 31 ngay")
+    break;
+    case "9":
+    console.log("thang 9 co 30 ngay")
+    break;
+    case "10":
+    console.log("thang 10 co 31 ngay");
+    break;
+    case "11":
+    console.log("thang 11 co 30 ngay")
+    break;
+    case "12":
+    console.log("thang 12 co 31 ngay")
+    break;
+    default:
+    console.log("Thang khong ton tai")
+    break
+}
